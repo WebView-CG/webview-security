38ef72451249d989117cb84a5e1eb33053f93566
Niklas Merz <niklasmerz@apache.org>
2026-07-05 20:30:31 +0200

    Convert Draft to Bikeshed

---
+++ README.md
@@ -0,0 +1,3 @@
+# Security Considerations
+
+This shall become a pratical for developers using WebViews on how to securely use them.
---
+++ build-remote.sh
@@ -0,0 +1,1 @@
+curl https://www.w3.org/publications/spec-generator/ -F file=@security-for-webviews.bs -F type=bikeshed-spec -F die-on=nothing > index.html
